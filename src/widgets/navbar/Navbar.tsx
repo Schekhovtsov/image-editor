9ee8458ac9c3a1198926600f4e37e731814987d5
Sergey Shehovcov <s.shehovcov@gk-adept.ru>
2025-01-27 12:28:24 +0300

    feat: headless-ui, navbar

--- src/widgets/navbar/Navbar.tsx
+++ src/widgets/navbar/Navbar.tsx
@@ -1,11 +1,26 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 import styles from './Navbar.module.scss';
 
 export const Navbar = () => {
     return (
-        <nav className={styles.container}>
-            <ul>
-                <li>File</li>
-            </ul>
-        </nav>
+        <div className={styles.container}>
+            <Menu>
+                <MenuButton className={styles.topButton}>Файл</MenuButton>
+                <MenuItems anchor="bottom" className={styles.dropdownItems}>
+                    <MenuItem>
+                        <a href="#">Создать</a>
+                    </MenuItem>
+                    <MenuItem>
+                        <a href="#">Открыть...</a>
+                    </MenuItem>
+                    <MenuItem disabled>
+                        <a href="#">Сохранить</a>
+                    </MenuItem>
+                    <MenuItem disabled>
+                        <a href="#">Сохранить как</a>
+                    </MenuItem>
+                </MenuItems>
+            </Menu>
+        </div>
     );
 };
