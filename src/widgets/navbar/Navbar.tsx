2f9a932c68b94f142507a083ed1d25802eeced57
Sergey Shehovcov <schekhovtsov@gmail.com>
2025-01-27 10:59:56 +0300

    feat: structure draft

---
+++ src/widgets/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import styles from './Navbar.module.scss';
+
+export const Navbar = () => {
+    return (
+        <nav className={styles.container}>
+            <ul>
+                <li>File</li>
+            </ul>
+        </nav>
+    );
+};
